1dac2ba3174fcfad8703cd8fd7952c8c591b7c61
Cole Davis <dylcdav@gmail.com>
2025-04-21 19:50:34 -0400

    Add component logic to ItemsRecipes

--- resource-planner/src/components/ItemsRecipes.vue
+++ resource-planner/src/components/ItemsRecipes.vue
@@ -1,12 +1,78 @@
 <script lang="ts">
+type Item = {
+    id: Number
+    name: String
+}
+
+type Recipe = {
+    id: Number
+    name: String
+    input: {
+        itemID: Number
+        quantity: Number
+    }
+    output: {
+        itemID: Number
+        quantity: Number
+    }
+}
+
 export default {
     name: 'ItemsRecipes',
+
+    data() {
+        return {
+            itemID: 0,
+            recipeID: 0,
+            itemName: '',
+            recipeName: '',
+            itemInputID: '',
+            itemInputQuantity: 0,
+            itemOutputID: '',
+            itemOutputQuantity: 0,
+            items: [] as Item[],
+            recipes: [] as Recipe[],
+        }
+    },
+
+    methods: {
+        addItem() {
+            this.items.push({
+                id: this.itemID,
+                name: this.itemName,
+            })
+
+            this.itemName = ''
+            this.itemID++
+        },
+        addRecipe() {
+            this.recipes.push({
+                id: this.recipeID,
+                name: this.recipeName,
+                input: {
+                    itemID: Number(this.itemInputID),
+                    quantity: this.itemInputQuantity,
+                },
+                output: {
+                    itemID: Number(this.itemOutputID),
+                    quantity: this.itemOutputQuantity,
+                },
+            })
+
+            this.recipeName = ''
+            this.recipeID++
+            this.itemInputID = ''
+            this.itemInputQuantity = 0
+            this.itemOutputID = ''
+            this.itemOutputQuantity = 0
+        },
+    },
 }
 </script>
 
 <template>
-    <!-- "this is too big and should be refactored" -->
-    <!-- correct. -->
+    <!-- "this is way too big and should be refactored immediately!!" -->
+    <!-- "yes." -->
     <div class="table-wrapper">
         <div>
             <h2>Items</h2>
@@ -20,16 +86,28 @@ export default {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>0</td>
+                        <td>{{ itemID }}</td>
                         <td>
                             <input
+                                v-model="itemName"
                                 type="text"
                                 placeholder="item name"
                             />
                         </td>
-                        <td><button type="button">Add</button></td>
+                        <td>
+                            <button
+                                type="button"
+                                @click="addItem"
+                                :disabled="itemName === ''"
+                            >
+                                Add Item
+                            </button>
+                        </td>
                     </tr>
-                    <tr></tr>
+                    <tr v-for="item in items">
+                        <td>{{ item.id }}</td>
+                        <td>{{ item.name }}</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
@@ -47,30 +125,81 @@ export default {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>0</td>
+                        <td>{{ recipeID }}</td>
                         <td>
                             <input
+                                v-model="recipeName"
                                 type="text"
                                 placeholder="recipe name"
                             />
                         </td>
                         <td>
-                            <select>
-                                <option>Select an Input</option>
+                            <select
+                                v-model="itemInputID"
+                                :disabled="items.length === 0"
+                            >
+                                <option value="">Select an Input</option>
+                                <option
+                                    v-for="item in items"
+                                    :key="item.id.toString()"
+                                    :value="item.id"
+                                >
+                                    {{ item.name }}
+                                </option>
                             </select>
-                            <input type="number" />
+                            <input
+                                type="number"
+                                v-model="itemInputQuantity"
+                            />
                         </td>
                         <td>
-                            <select>
-                                <option>Select an Output</option>
+                            <select
+                                v-model="itemOutputID"
+                                :disabled="items.length === 0"
+                            >
+                                <option value="">Select an Input</option>
+                                <option
+                                    v-for="item in items"
+                                    :key="item.id.toString()"
+                                    :value="item.id"
+                                >
+                                    {{ item.name }}
+                                </option>
                             </select>
-                            <input type="number" />
+                            <input
+                                type="number"
+                                v-model="itemOutputQuantity"
+                            />
                         </td>
                         <td>
-                            <button type="button">Add Recipe</button>
+                            <button
+                                type="button"
+                                @click="addRecipe"
+                                :disabled="
+                                    itemInputID === '' ||
+                                    itemOutputID === '' ||
+                                    itemInputQuantity === 0 ||
+                                    itemOutputQuantity === 0
+                                "
+                            >
+                                Add Recipe
+                            </button>
                         </td>
                     </tr>
-                    <tr></tr>
+                    <tr v-for="recipe in recipes">
+                        <td>{{ recipe.id }}</td>
+                        <td>{{ recipe.name }}</td>
+                        <td>
+                            {{ items.find(({ id }) => id === recipe.input.itemID)?.name }} ({{
+                                recipe.input.quantity
+                            }}x)
+                        </td>
+                        <td>
+                            {{ items.find(({ id }) => id === recipe.output.itemID)?.name }} ({{
+                                recipe.output.quantity
+                            }}x)
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
