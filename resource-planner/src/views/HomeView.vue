cc8150fadf7c63de435c371969cd83ca1ce11fc6
Cole Davis <dylcdav@gmail.com>
2025-04-21 19:21:14 -0400

    Add ItemsRecipes to HomeView

--- resource-planner/src/views/HomeView.vue
+++ resource-planner/src/views/HomeView.vue
@@ -2,15 +2,19 @@
     <BaseLayout>
         <h2>Welcome to Recipe Planner</h2>
         <span>Please begin by uploading a recipe below.</span>
+        <ItemsRecipes />
     </BaseLayout>
 </template>
 
 <script lang="ts">
 import BaseLayout from '../components/BaseLayout.vue'
+import ItemsRecipes from '@/components/ItemsRecipes.vue'
+
 export default {
     name: 'HomeView',
     components: {
         BaseLayout,
+        ItemsRecipes,
     },
 }
 </script>
