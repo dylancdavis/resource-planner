8ea8d7221e73b243607510f3c1dfe767fb30cce3
Cole Davis <dylcdav@gmail.com>
2025-06-01 21:04:12 +0000

    Add reduceToBase

---
+++ src/utils/recipes.ts
@@ -0,0 +1,6 @@
+import type { Counter } from "./counter";
+import type { Recipe } from "@/components/ItemsRecipes.vue";
+
+function reduceToBase(counter: Counter, recipes: Recipe[]): Counter {
+	return counter
+}
